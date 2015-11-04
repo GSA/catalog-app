a95f800ccfd222dc5ea90cda1763c3965efc38e0
Dan Olaru <dan.olaru@reisystems.com>
2015-11-04 16:50:28 -0500

    add pybuild support

---
+++ build.py
@@ -0,0 +1,13 @@
+from pybuilder.core import init, use_plugin
+
+use_plugin("python.core")
+use_plugin("python.install_dependencies")
+use_plugin("exec")
+
+default_task = ["install_dependencies", "publish"]
+
+@init
+def initialize(project):
+    project.set_property("dir_source_main_python", "ckan")
+    project.depends_on_requirements("requirements.txt")
+    #project.set_property("publish_command", "sh install.sh")
--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 set -e
 
-VIRTUAL_ENV=/usr/lib/ckan
+VIRTUAL_ENV=ckan
+EXTENSIONS=$(cat requirements.txt | grep -o "egg=.*" | cut -f2- -d'=')
 
-# create virtual_env
-virtualenv $VIRTUAL_ENV --no-site-packages
-
-# switch to virtual env
-. $VIRTUAL_ENV/bin/activate
-
-# install ckan core + ckan extensions
-pip install -r requirements.txt
-
-cat requirements.txt | grep -o "egg=.*" | cut -f2- -d'=' | xargs -I % \
-    sh -c 'pip install -r $VIRTUAL_ENV/src/%/requirements.txt ; \
-    cd $VIRTUAL_ENV/src/%/;
-    $VIRTUAL_ENV/bin/python setup.py develop;'
+# install/setup each extension individually
+for extension in $EXTENSIONS; do
+    if [ -f $VIRTUAL_ENV/src/$extension/requirements.txt ]; then 
+        $VIRTUAL_ENV/bin/pip install -r $VIRTUAL_ENV/src/$extension/requirements.txt
+    elif [ -f $VIRTUAL_ENV/src/$extension/pip-requirements.txt ]; then
+    	$VIRTUAL_ENV/bin/pip install -r $VIRTUAL_ENV/src/$extension/pip-requirements.txt
+    fi
+done
