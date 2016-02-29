b2b683288386bfe950179ec095fff229af83eb88
JJediny <John.Jediny@gsa.gov>
2016-02-28 21:40:18 -0500

    pyCSW is running thanks @tomkralidis

--- docker/pycsw/install_pycsw.sh
+++ docker/pycsw/install_pycsw.sh
@@ -2,10 +2,9 @@
 
 # Install PyCSW
 
-CSWDIR=/var/www/
-mkdir -p $CSWDIR && cd $CSWDIR
+mkdir -p /var/www/pycsw && cd /var/www/pycsw
 git clone https://github.com/geopython/pycsw.git
-cd $CSWDIR/pycsw && pip install -e . && pip install -r requirements.txt
-cp -a /tmp/pycsw-all.cfg $CSWDIR/pycsw
-rm -R /tmp/pycsw-all.cfg
+cd /var/www/pycsw && pip install -e . && pip install -r requirements.txt
+cp -a /tmp/default.cfg /var/www/pycsw/default.cfg
+rm -R /tmp/default.cfg
 exit
