c4b2e779ae383d05d4b5f9ec26c555bbd0fb5dfa
JJediny <John.Jediny@gsa.gov>
2016-02-29 14:20:52 -0500

    Updates to README thanks @dportnoy closes #23 #23

--- docker/pycsw/install_pycsw.sh
+++ docker/pycsw/install_pycsw.sh
@@ -2,9 +2,12 @@
 
 # Install PyCSW
 
-mkdir -p /var/www/pycsw && cd /var/www/pycsw
+mkdir -p $PYCSW_WEB/pycsw && cd $PYCSW_WEB/pycsw
 git clone https://github.com/geopython/pycsw.git
-cd /var/www/pycsw && pip install -r requirements.txt && pip install -e .
-cp -a /tmp/default.cfg /var/www/pycsw/default.cfg
+cd $PYCSW_WEB/pycsw && pip install -r requirements.txt && pip install -e .
+cp -a /tmp/default.cfg $PYCSW_CONFIG/default.cfg
+cp -a /tmp/pycsw $PYCSW_CRON/pycsw
+cp -a /tmp/pycsw.wsgi $PYCSW_WEB/pycsw.wsgi
 rm -R /tmp/default.cfg
+rm -R /tmp/pycsw.wsgi
 exit
