a65eff960b0ac449ddde3c0452518e5729aff834
Tom Wood <tom.wood@civicactions.com>
2020-04-21 16:52:09 -0400

    Move PostGIS SQL into separate SQL file.

--- configure-postgis.py
+++ configure-postgis.py
@@ -16,27 +16,17 @@ def identifier(s):
     """
     return u'"' + s.replace(u'"', u'""').replace(u'\0', '') + u'"'
 
-def postgis_sql(user):
     return """
-        CREATE EXTENSION IF NOT EXISTS postgis;
-        CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-        CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
-        CREATE EXTENSION IF NOT EXISTS postgis_topology;
-        ALTER SCHEMA tiger OWNER TO {user};
-        ALTER SCHEMA tiger_data OWNER TO {user};
-        ALTER SCHEMA topology OWNER TO {user};
-        CREATE FUNCTION exec(text) RETURNS text LANGUAGE PLPGSQL VOLATILE AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;                              
-        SELECT exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.relname) || ' OWNER TO {user};')
-            FROM (
-                SELECT nspname, relname
-                FROM pg_class c JOIN pg_namespace n ON (c.relnamespace = n.oid) 
-                WHERE nspname IN ('tiger','topology', 'tiger_data') AND
-            relkind IN ('r','S','v') ORDER BY relkind = 'S')
-        s;  
 
-        SET search_path=public,tiger;
-
-    """.format(user=identifier(user))
+def postgis_sql(user):
+    """
+    Return some SQL to configure the PostGIS extension.
+    """
+    
+    template_filename = 'configure-postgis.sql'
+    with open(template_filename) as fp:
+        template = fp.read()
+    return template.format(user=identifier(user))
 
 def get_env(name):
     if name not in os.environ:
