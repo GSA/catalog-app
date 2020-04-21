6ca2a0eafbb6b01363af42f5f55a931afd5be3dd
Tom Wood <tom.wood@civicactions.com>
2020-04-21 15:38:33 -0400

    Fixes after losing local docker invocation

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -4,14 +4,8 @@
 # specified in vendor-requirements.txt into the "vendor" directory.
 
 # Install any packaged dependencies for our vendored packages
-apt-get -y update
-apt-get -y install swig build-essential python-dev libssl-dev libgeos-dev
+sudo apt-get -y update
+sudo apt-get -y install swig build-essential python-dev libssl-dev libgeos-dev
 
-# Install PIP
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-python /tmp/get-pip.py
-
-# As the VCAP user, cache .whls based on the frozen requirements for vendoring
-cd /tmp/app 
-mkdir vendor
+# Cache wheels for requirements
 pip wheel -r requirements-freeze.txt -w vendor
