7df3df782941da2275c46c9933e8702f1484f0e1
Tom Wood <tom.wood@civicactions.com>
2020-04-21 15:54:11 -0400

    Set CF API endpoint

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
-# This script installs some necessary .deb dependencies and then installs binary .whls for the packages
-# specified in vendor-requirements.txt into the "vendor" directory.
+# This script installs some necessary .deb dependencies and then
+# installs binary .whls for the packages specified in
+# vendor-requirements.txt into the "vendor" directory.
 
 # Install any packaged dependencies for our vendored packages
 sudo apt-get -y update
