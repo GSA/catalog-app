a21e427f0b40ac4eb573f9b45a78728cc4b8ee90
Aaron D Borden <adborden@a14n.net>
2020-12-04 17:48:45 -0800

    wip

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -2,4 +2,6 @@
 
 . /app/.venv/bin/activate
 
+sudo Xvfb -ac $DISPLAY -screen 0 1280x1024x16 &
+
 exec "$@"
--- test/test_dataset_page.py
+++ test/test_dataset_page.py
@@ -1,8 +1,45 @@
+import httplib
+import socket
+import time
+
 from seleniumbase import BaseCase
 
-class MyTestClass(BaseCase):
+def wait_for_ckan(host='app', port=80, url='/api/action/package_search?rows=0', timeout=60):
+    start = time.time()
 
-    def test_basic(self):
+    while time.time() < start + timeout:
+        print 'trying %s' % url
+        response = None
+        try:
+            c = httplib.HTTPConnection(host, port, timeout=5)
+            c.request('GET', url)
+            response = c.getresponse()
+        except httplib.NotConnected as e:
+            print 'error %s' % e
+            pass
+        except socket.error as e:
+            print 'error %s' % e
+            pass
+
+        if response and response.status == 200:
+            print 'success!'
+            break
+
+        time.sleep(1)
+
+
+class MyTestClass(BaseCase):
+    @classmethod
+    def setUpClass(cls):
+        #wait_for_ckan(host='localhost', port=8080)
+        wait_for_ckan()
+
+    def test_ckan(self):
+        #self.open('http://localhost:8080/dataset')
+        self.open('http://app/dataset')
+        self.assert_text('DATA CATALOG')
+
+    def test_xkcd(self):
         self.open("https://store.xkcd.com/search")
         self.type('input[name="q"]', "xkcd book")
         self.click('input[value="Search"]')
